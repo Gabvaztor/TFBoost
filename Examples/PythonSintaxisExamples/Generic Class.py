1fb5fdc93d097b23cfdce6481cb2f0f431002340
Gabvaztor <gabvaztor@gmail.com>
2017-03-05 22:52:43 +0100

    First commit. Push proyect's arquitecture and the starting code
    This commit is the first commit. It contains 10 files which can provide the first step of this proyect with comments.
    TFCode.py is the most important file and it will containts the most important code of the proyect.
    See next commits for more information.

--- Examples/PythonSintaxisExamples/Generic Class.py
+++ Examples/PythonSintaxisExamples/Generic Class.py
@@ -7,24 +7,20 @@ class Objeto(object): #clase genérica
 
     def printAtributos(self):
         print("El objeto con nombre " + self.nombre +
-              " tiene " + str(self.edad) +
-              " años de edad y " + str(self.ojos) + " ojos")
+              " tiene " + self.edad + " años de edad y " + self.ojos + " ojos")
 
 class Animal(Objeto): #clase no genérica
     ojos = None
     def __init__(self,nombre,edad,ojos):
-        Objeto.__init__(self, nombre,edad)
+        super(Objeto,self).__init__(nombre,edad)
         self.ojos = ojos
 
 class Humano(Animal): #clase no genérica
     inteligencia = None
     nacionalidad = None
     def __init__(self,nombre,edad,ojos,inteligencia,nacionalidad):
-        Animal.__init__(self,nombre,edad,ojos)
+        super(Animal, self).__init__(nombre,edad,ojos)
         self.inteligencia = inteligencia
-
-a = Animal("hola", 24, 3)
-a.printAtributos()
 
 maria = Humano("María",25,2,90,"Española")
 maria.printAtributos( )
