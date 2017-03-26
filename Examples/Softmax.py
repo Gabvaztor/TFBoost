fa8639a8fcc08c3bbe1e2ad7814d8992ed4286ef
gabvaztor <gabvaztor@gmail.com>
2017-03-26 22:18:01 +0200

    Move Softmax file

--- Examples/Softmax.py
+++ Examples/Softmax.py
@@ -8,5 +8,6 @@ import numpy as np
 def softmax(x):
     return np.exp(x)/ np.sum(np.exp(x),axis=0)
 
+
 scores = [1.0, 2.0]
 print (softmax(scores))
--- UsefulTools/Softmax.py
+++
@@ -1,6 +0,0 @@
-def softmax(x):
-    return np.exp(x)/ np.sum(np.exp(x),axis=0)
-
-
-scores = [1.0, 2.0]
-print (softmax(scores))
