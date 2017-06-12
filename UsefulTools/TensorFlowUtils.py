dbff023b4bddf1a70f7acab5c1f6c2c73928db2b
gabvaztor <gabvaztor@gmail.com>
2017-06-12 02:40:19 +0200

    Now TFModels has a new structure to train neuronal networks

--- UsefulTools/TensorFlowUtils.py
+++ UsefulTools/TensorFlowUtils.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 import tensorflow as tf
+
 
 def weighted_mape_tf(y_true, y_prediction):
     tot = tf.reduce_sum(y_true)
@@ -25,7 +25,7 @@ def weight_variable(shape):
 
 
 def bias_variable(shape):
-    initial = tf.truncated_normal(shape=shape, stddev=0.01)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
 
@@ -38,17 +38,8 @@ def max_pool_2x2(x):
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
-def initialize_session(debug_level=None):
-    """
-    Initialize interactive session and all local and global variables
-    :return: Session
-    """
-    if debug_level == 3 or debug_level == 2 or debug_level == 1 :
-        config = tf.ConfigProto(allow_soft_placement=True,
-                                log_device_placement=True)
-    else:
-        config = tf.ConfigProto(allow_soft_placement=True)
-    sess = tf.InteractiveSession(config=config)
+def initialize_session():
+    sess = tf.InteractiveSession()
     sess.run(tf.local_variables_initializer())
     sess.run(tf.global_variables_initializer())
     return sess
