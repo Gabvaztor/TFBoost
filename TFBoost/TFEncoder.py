fb0104316788aad4a8a43c7ef25660758254c697
gabvaztor <gabvaztor@gmail.com>
2018-06-15 19:27:23 +0200

    Data Generation with benchmarks v3

--- TFBoost/TFEncoder.py
+++ TFBoost/TFEncoder.py
@@ -53,6 +53,8 @@ class Dictionary(object):
     string_labels_type_option_hierarchy = 'hierarchy'
     # Format png
     string_extension_png = '.png'
+    # Format png
+    string_extension_jpeg = '.jpeg'
     # Format ckpt
     string_ckpt_extension = '.ckpt'
     # Format meta
@@ -104,6 +106,7 @@ class Dictionary(object):
 
     string_settings_web_traffic = "../Settings/Web_Traffic_Time/SETTINGS.json"
 
+    string_settings_retinopathy_k = "../Settings/Retinopathy_k/SETTINGS.json"
     """
     STRING FILENAMES
     """
@@ -129,7 +132,8 @@ class Dictionary(object):
     string_option_web_traffic_problem = 'web_traffic_problem'
     # Option Breast Cancer Wisconsin problem
     string_breast_cancer_wisconsin_problem = 'web_traffic_problem'
-
+    # Option Retinopathy_K problem
+    string_retinopathy_k_problem = "retinopathy_k_problem"
 
 class Errors(object):
     """
